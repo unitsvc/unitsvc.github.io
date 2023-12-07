dbef359d932e206e32b47de84762ca89f9c8c068
聚合服务平台 <unitsvc@gmail.com>
2023-12-07 09:16:18 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,22 @@
+#!/usr/bin/env sh
 
+# enable throw error
+set -e
+
+# build static pages
+npm run build
+
+# enter build
+cd build
+
+# publish pages
+git init
+git add -A
+git commit -m 'deploy'
+git push -f https://github.com/unitsvc/unitsvc.github.io.git master:gh-pages
+
+# switch origin directory
+cd -
+
+# remove build pages
+rm -rf ./build
